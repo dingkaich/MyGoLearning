d6e85e6532ea6a07c30f9909eff5f5a50d35d783
dingkaich <dingkaich@163.com>
2016-12-23 02:17:28 +0800

    my first commit

---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"myprint"
+)
+
+func main() {
+	myprint.Myprint()
+}
---
+++ src/myprint/myprint.go
@@ -0,0 +1,14 @@
+package myprint
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("i am init 1")
+}
+
+func Myprint() {
+
+	fmt.Println("123")
+}
