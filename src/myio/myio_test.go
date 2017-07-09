26c050daa5452d7877baca12acdcd307b4fe444d
dingkaich <dingkaich@163.com>
2017-07-09 21:51:42 +0800

    https://github.com/cweill/gotests gotest满好用的

---
+++ src/myio/myio_test.go
@@ -0,0 +1,15 @@
+package myio
+
+import "testing"
+
+func TestMyioMain(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{"dingkai"},
+	}
+	for range tests {
+		MyioMain()
+	}
+}
---
+++ src/mytime/mytime_test.go
@@ -0,0 +1,16 @@
+package mytime
+
+import "testing"
+
+func Test_timer1(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{"dingkai"},
+	}
+	for range tests {
+
+		timer1()
+	}
+}
